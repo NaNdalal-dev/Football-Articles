278d64c94c300e4a0e85b19c78e71124158f3d71
NaNdalal-dev <dnandalal7@gmail.com>
2021-04-19 22:51:35 +0530

    Commit 3

--- FootballArticles/ArticleApp/urls.py
+++ FootballArticles/ArticleApp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, include
 from .views import*
 urlpatterns = [
 	path('Transfers/',Transfers,name='transfers'),
+	path('Transfers/<str:link>/',TransfersWhole,name='transferswhole'),
+	
 	path('Official/',Official,name='official'),
-	path('HotTopic/',HotTopic,name='hottop')
+	path('Official/<str:link>/',OfficialWhole,name='OfficialWhole'),
+
+	path('HotTopic/',HotTopic,name='hottop'),
+	path('HotTopic/<str:link>/',HotTopicWhole,name='hottopWhole')
 ]
--- FootballArticles/ArticleApp/views.py
+++ FootballArticles/ArticleApp/views.py
@@ -2,16 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import*
 
-# Create your views here.
+# Official Views
 def Official(request):
-	return render(request, 'official.html')
+	data2 = OfficialArticle.objects.all().order_by('Time')[::-1]
+	data = data2
+	return render(request, 'official.html',{'allData':data})
 
-def Transfers(request):
-	data2 = TransfersArticle.objects.get
-	data = data2()
+def OfficialWhole(request,link):
+	data = OfficialArticle.objects.get(id=link)
 	data.Article = data.Article.split('\n')
-	print(data.Article)
-	return render(request, 'Transfers.html',{'data':data})
+	return render(request,'fullArticle.html',{'data':data})
 
+# Transfers Views
+def Transfers(request):
+	data2 = TransfersArticle.objects.all().order_by('Date')[::-1]
+	data = data2
+	return render(request, 'Transfers.html',{'allData':data})
+
+def TransfersWhole(request,link):
+	data = TransfersArticle.objects.get(id=link)
+	data.Article = data.Article.split('\n')
+	return render(request,'fullArticle.html',{'data':data})
+
+# HotTopic Views
 def HotTopic(request):
-	return render(request, 'hottopic.html')
+	data2 = HotTopicArticle.objects.all().order_by('Time')[::-1]
+	data = data2
+	return render(request, 'hottopic.html',{'allData':data})
+
+def HotTopicWhole(request,link):
+	data = HotTopicArticle.objects.get(id=link)
+	data.Article = data.Article.split('\n')
+	return render(request,'fullArticle.html',{'data':data})
